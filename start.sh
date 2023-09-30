eaeb0fff3c035b1e27e51a69272847a6e5d1b60d
Mikko Partio <mikko.partio@fmi.fi>
2023-09-30 10:18:16 +0300

    Suspend suites unless environment is prod

--- start.sh
+++ start.sh
@@ -6,15 +6,27 @@ mkdir -p $ECF_HOME && cd $ECF_HOME
 # ecflow_start.sh -p $ECF_PORT
 
 nohup ecflow_server > $ECF_HOME/ecflow.log 2>&1 < /dev/null &
+
 sleep 5 # wait for server start
-ecflow_client --restart
+
 create-defs.sh 
 load-defs.sh
 
 set +u
 
+# Suspend all suites if environment is not prod
+if [ -n "$ENVIRONMENT" ] && [ "$ENVIRONMENT" != "prod" ]; then
+  for suite in $(ecflow_client --suites); do
+    ecflow_client --suspend /$suite
+  done
+fi
+
+# Set server variables if they exists in the environment
 for v in "ECF_RESTAPI_HOST" "ECF_RESTAPI_PORT" "ECF_LOGHOST" "ECF_LOGPORT" "FMI_ENV"; do
   eval test "x" != x\$$v && eval ecflow_client --alter add variable "$v" "\$$v" /
 done
+
+# Place server in restart mode
+ecflow_client --restart
 
 sleep infinity
